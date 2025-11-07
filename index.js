25dd293ba4c27c944e967abebb9d200ab0081d0f
Alessio Michelini <amichelini@hubspot.com>
2025-11-07 11:57:48 +0000

    refactor: migrate ESLint configuration to ES module format and update dependencies
    - Converted ESLint configuration in index.js to use ES module syntax.
    - Updated ESLint and TypeScript-related dependencies in package.json and package-lock.json.
    - Adjusted ESLint rules and ignore patterns for better compatibility and maintainability.

--- index.js
+++ index.js
@@ -1,44 +1,60 @@
-module.exports = {
-  env: {
-    commonjs: true,
-    es2021: true,
-    node: true,
-  },
-  ignorePatterns: ['/node_modules/', '/.serverless/', '/.eslint.config.js', '/.webpack/'],
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
-  rules: {
-    'no-console': [
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    env: {
+      node: true,
+      es2022: true,
+    },
+    plugins: { js },
+    extends: ['js/recommended'], 
+    languageOptions: {
+      globals: {...globals.node}
+    },
+    ignores: [
+      '**/node_modules/**',
+      '**/.serverless/**',
+      '**/.webpack/**',
+      '**/dist/**',
+      'eslint.config.js'
+    ],
+    rules: {
+      'no-console': [
       'error',
-      {
-        allow: ['info', 'warn', 'error'],
-      },
-    ],
-    camelcase: 0,
-    'comma-dangle': 1,
-    'arrow-parens': 0,
-    'no-plusplus': 0,
-    'no-underscore-dangle': 0,
-    'no-confusing-arrow': 0,
-    'import/no-unresolved': 0,
-    'import/prefer-default-export': 0,
-    'no-trailing-spaces': [2, { skipBlankLines: true }],
-    'no-unused-expressions': [1, { allowTernary: true }],
-    'max-len': [
-      2,
-      {
-        code: 120,
-        ignoreStrings: true,
-        ignoreTemplateLiterals: true,
-      },
-    ],
-    'operator-linebreak': 0,
-    'implicit-arrow-linebreaks': 0,
-    'implicit-arrow-linebreak': 0,
-    'object-curly-newline': 0,
-    'newline-per-chained-call': 0,
-    indent: 0,
-    'function-paren-newline': 0,
+        {
+          allow: ['info', 'warn', 'error'],
+        },
+      ],
+      camelcase: 'off',
+      'comma-dangle': ['warn', 'always-multiline'],
+      'arrow-parens': 0,
+      'no-plusplus': 0,
+      'no-underscore-dangle': 0,
+      'no-confusing-arrow': 0,
+      'import/no-unresolved': 0,
+      'import/prefer-default-export': 0,
+      'no-trailing-spaces': ['error', { skipBlankLines: true }],
+      'no-unused-expressions': ['warn', { allowTernary: true }],
+      'max-len': [
+        2,
+        {
+          code: 120,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+        },
+      ],
+      'operator-linebreak': 0,
+      'implicit-arrow-linebreaks': 0,
+      'implicit-arrow-linebreak': 0,
+      'object-curly-newline': 0,
+      'newline-per-chained-call': 0,
+      indent: 0,
+      'function-paren-newline': 0,
+    }
   },
-};
+  tseslint.configs.recommended,
+]);
