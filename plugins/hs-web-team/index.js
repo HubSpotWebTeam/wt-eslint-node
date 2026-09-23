6c84c48d2d89a8c08d237836d7789e23241ae01f
David Ding <davidftding@gmail.com>
2026-09-23 15:46:25 +0100

    feat: add hs-web-team/no-non-literal-constant-case rule (#73)

--- plugins/hs-web-team/index.js
+++ plugins/hs-web-team/index.js
@@ -1,9 +1,11 @@
 import { noAbbreviations } from './rules/no-abbreviations.js';
+import { noNonLiteralConstantCase } from './rules/no-non-literal-constant-case.js';
 import { noReduceAccumulatorCopy } from './rules/no-reduce-accumulator-copy.js';
 
 export const hsWebTeamPlugin = {
   rules: {
     'no-abbreviations': noAbbreviations,
+    'no-non-literal-constant-case': noNonLiteralConstantCase,
     'no-reduce-accumulator-copy': noReduceAccumulatorCopy,
   },
 };
@@ -15,6 +17,7 @@ hsWebTeamPlugin.configs = {
     plugins: { 'hs-web-team': hsWebTeamPlugin },
     rules: {
       'hs-web-team/no-abbreviations': 'warn',
+      'hs-web-team/no-non-literal-constant-case': 'warn',
       'hs-web-team/no-reduce-accumulator-copy': 'error',
     },
   },
